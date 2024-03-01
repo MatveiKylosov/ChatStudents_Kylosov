9e43fa5cd48d147cec88b8f124907b9b130202ad
MatveiKylosov <matveikyl@mail.ru>
2024-03-01 20:10:52 +0500

    Разработанные контексты данных

---
+++ Classes/MessagesContext.cs
@@ -0,0 +1,16 @@
+﻿using ChatStudents_Kylosov.Classes.Common;
+using ChatStudents_Kylosov.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatStudents_Kylosov.Classes
+{
+    public class MessagesContext : DbContext
+    {
+        public DbSet<Messages> Messages { get; set; }
+        public MessagesContext() => Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(Config.config);
+    }
+}
---
+++ Classes/UsersContext.cs
@@ -0,0 +1,16 @@
+﻿using ChatStudents_Kylosov.Classes.Common;
+using ChatStudents_Kylosov.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatStudents_Kylosov.Classes
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public UsersContext() => Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(Config.config);
+    }
+}
