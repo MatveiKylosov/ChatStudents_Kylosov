29d4bde779144aa0d48a89005c5d1d1a716939ce
MatveiKylosov <matveikyl@mail.ru>
2024-03-02 18:11:22 +0500

    Разработанный пользовательский элемент Message. Kylosov

---
+++ Pages/Items/Message.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ChatStudents_Kylosov.Classes.Common;
+using ChatStudents_Kylosov.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatStudents_Kylosov.Pages.Items
+{
+    /// <summary>
+    /// Логика взаимодействия для Message.xaml
+    /// </summary>
+    public partial class Message : UserControl
+    {
+        public Message(Messages messages, Users UserFrom)
+        {
+            InitializeComponent();
+
+            imgUser.Source = BitmapFromArrayByte.LoadImage(UserFrom.Photo);
+            FIO.Content = UserFrom.ToFIO();
+            tbMessage.Text = messages.Message;
+        }
+    }
+}
